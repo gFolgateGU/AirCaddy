0b9298b7a46111bf7c7528a93f8f7dd7a07611d0
gFolgateGU <gfolgate@outlook.com>
2017-11-29 17:12:07 -0500

    Added ASP.NET MVC Application Base

---
+++ AirCaddy_Web/AirCaddy_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AirCaddy_Web.MvcApplication" Language="C#" %>
---
+++ AirCaddy_Web/AirCaddy_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirCaddy_Web.Startup))]
+namespace AirCaddy_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
