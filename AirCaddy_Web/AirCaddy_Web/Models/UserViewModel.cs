ecb52c833607f135f4189dc134ef156bf2ab927c
gFolgateGU <gfolgate@outlook.com>
2018-01-10 11:22:41 -0500

    Reset Project Structure

---
+++ AirCaddy_Web/AirCaddy_Web/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirCaddy_Web.Models
+{
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
