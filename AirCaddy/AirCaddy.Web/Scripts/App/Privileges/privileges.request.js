8344f20dfd6cc05be7821581f85a7113ea6ff089
gFolgateGU <gfolgate@outlook.com>
2018-01-25 15:36:19 -0500

    closing out branch touch ups

--- AirCaddy/AirCaddy.Web/Scripts/App/Privileges/privileges.request.js
+++ AirCaddy/AirCaddy.Web/Scripts/App/Privileges/privileges.request.js
@@ -1,1 +1,12 @@
-﻿
+﻿var request = function(data) {
+    var self = this;
+    
+    self.id = data.Id;
+    self.username = data.UserName;
+    self.courseName = data.GolfCourseName;
+    self.courseAddress = data.GolfCourseAddress;
+    self.phoneNumber = data.CoursePhoneNumber;
+    self.courseType = data.GolfCourseType;
+    self.reason = data.Reason;
+    self.verified = data.Verified;
+}
