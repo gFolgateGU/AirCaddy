3ddf58c862dc0faf9ce4ddd546a23ef6a9eb70b3
gFolgateGU <gfolgate@outlook.com>
2018-02-07 19:44:34 -0500

    Started build course

---
+++ AirCaddy/AirCaddy.Domain/Special/CourseBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCaddy.Data;
+using AirCaddy.Data.Repositories;
+using AirCaddy.Domain.Services.GolfCourses;
+
+namespace AirCaddy.Domain.Special
+{
+    public abstract class CourseBuilder
+    {
+        protected GolfCourse _golfCourse;
+        protected IYelpGolfCourseReviewService _yelpGolfCourseReviewService;
+        protected IPrivilegeRepository _privilegeRepository;
+
+        protected CourseBuilder()
+        {
+            _golfCourse = new GolfCourse();
+        }
+
+        protected abstract void BuildCourse(int requestId);
+
+        protected GolfCourse GetCourse()
+        {
+            return _golfCourse;
+        }
+
+    }
+}
---
+++ AirCaddy/AirCaddy.Domain/Special/GolfCourseBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCaddy.Data.Repositories;
+using AirCaddy.Domain.Services.GolfCourses;
+
+namespace AirCaddy.Domain.Special
+{
+    public class GolfCourseBuilder : CourseBuilder
+    {
+        private readonly IPrivilegeRepository _privilegeRepository;
+        private readonly IYelpGolfCourseReviewService _yelpGolfCourseReviewService;
+
+        public GolfCourseBuilder(IPrivilegeRepository privilegeRepository,
+            IYelpGolfCourseReviewService yelpGolfCourseReviewService) : base()
+        {
+            _privilegeRepository = privilegeRepository;
+            _yelpGolfCourseReviewService = yelpGolfCourseReviewService;
+        }
+
+        protected override void BuildCourse(int requestId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
