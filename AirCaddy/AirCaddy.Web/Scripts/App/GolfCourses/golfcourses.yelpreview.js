2c70ca6c04a20fdb11fd3298fae77108f04b350d
gFolgateGU <gfolgate@outlook.com>
2018-02-13 23:12:10 -0500

    fixed shitty landing page for explore

--- AirCaddy/AirCaddy.Web/Scripts/App/GolfCourses/golfcourses.yelpreview.js
+++ AirCaddy/AirCaddy.Web/Scripts/App/GolfCourses/golfcourses.yelpreview.js
@@ -1,8 +1,19 @@
 ﻿var yelpReview = function(reviewData) {
     var self = this;
 
+    self.reviewVisibleFlag = ko.observable(false);
+
     self.rating = reviewData.Rating;
     self.user = reviewData.User;
     self.reviewText = reviewData.ReviewText;
     self.reviewDate = reviewData.ReviewDate;
+
+    self.toggleReview = function () {
+        if (!self.reviewVisibleFlag()) {
+            self.reviewVisibleFlag(true);
+        }
+        else {
+            self.reviewVisibleFlag(false);
+        }
+    }
 }
