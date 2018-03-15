507feacc19dfac7bce832ccb46c90f4d459f9913
gFolgateGU <gfolgate@outlook.com>
2018-03-15 16:11:42 -0400

    starting sendgrid stuff

--- AirCaddy/AirCaddy.Web/App_Start/IdentityConfig.cs
+++ AirCaddy/AirCaddy.Web/App_Start/IdentityConfig.cs
@@ -11,6 +11,12 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using AirCaddy.Root_Models;
+using SendGrid;
+using System.Net;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Mail;
+using SendGrid.Helpers.Mail;
 
 namespace AirCaddy
 {
@@ -20,6 +26,21 @@ namespace AirCaddy
         {
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
+        }
+
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
         }
     }
 
--- AirCaddy/AirCaddy.Web/Controllers/HomeController.cs
+++ AirCaddy/AirCaddy.Web/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using AirCaddy.Domain.Services.GolfCourses;
 using AirCaddy.Domain.ViewModels.GolfCourses;
 using Newtonsoft.Json.Linq;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace AirCaddy.Controllers
 {
@@ -28,6 +30,15 @@ namespace AirCaddy.Controllers
 
         public async Task<ActionResult> About()
         {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("ejbyrd@hotmail.com", "Hey Mer - Right Dude Here");
+            var subject = "I am currently SENDING IT with C# SendGrid";
+            var to = new EmailAddress("morse005@knights.gannon.edu", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            string htmlContent = "<strong>yeaboiwemadeit</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
 
             return View();
         }
