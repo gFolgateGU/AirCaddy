d6cd023aea9f6832900d58f95d9573ac831fc47e
Unknown <vero001@knights.gannon.edu>
2018-04-25 11:39:59 -0400

    Home page is doe muther fckers

--- AirCaddy/AirCaddy.Web/Root_Models/IdentityModels.cs
+++ AirCaddy/AirCaddy.Web/Root_Models/IdentityModels.cs
@@ -27,7 +27,7 @@ namespace AirCaddy.Root_Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("Abc", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
